e59e3976a47267a5d82a01758838062774d4f523
Nishtha Singh <75165796+nishtha2001@users.noreply.github.com>
2022-05-09 23:38:38 +0530

    Time: 3 ms (38.29%), Space: 7.4 MB (5.38%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -1,15 +1,37 @@
 class Solution {
 public:
-    vector<string> letterCombinations(string digits) {
-	if(digits == "") return {};
-	vector<string> mappings{"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"}, ans{""};
-	for(auto digit : digits){
-		vector<string> extendCombination;
-		for(auto& currentCombination : ans)
-			for(auto it : mappings[digit - '2'])
-				extendCombination.push_back(currentCombination + it);                            
-		ans = move(extendCombination); 
-	}
-	return ans;
+vector<string> letterCombinations(string digits) {
+    vector<string> ans;        
+    string curr;                
+    if(digits.length() == 0) return ans;    
+    
+    backtrack(digits, 0, curr, ans);
+    return ans;
+    
 }
+
+void backtrack(string digits, int i, string curr, vector<string> &ans) {   
+    unordered_map<char, string> mp = {          
+        {'2', "abc"},
+        {'3', "def"},
+        {'4', "ghi"},
+        {'5', "jkl"},
+        {'6', "mno"},
+        {'7', "pqrs"},
+        {'8', "tuv"},
+        {'9', "wxyz"},
+    };
+    if(i == digits.length()) {          
+        ans.push_back(curr);            
+        return;               
+    }
+    
+    for(auto c : mp[digits[i]]){           
+        curr.push_back(c);
+        backtrack(digits, i+1, curr, ans);
+        curr.pop_back();         
+    }
+   
+}
+
 };
