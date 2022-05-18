3756db3a72eb5d0fd00b0b15a6dd4fba7261a23d
Nishtha Singh <75165796+nishtha2001@users.noreply.github.com>
2022-05-18 22:37:43 +0530

    Time: 747 ms (68.76%), Space: 180.6 MB (50.50%) - LeetHub

---
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+  int time;
+  vector<int> disc, lowlink, parent;   
+  vector<vector<int>> g, answer;
+  
+  void dfs(int node){
+    disc[node] = time;
+    lowlink[node] = time++;
+  
+    int apexes = 0;
+    for(auto &nei: g[node])
+      if(disc[nei] == -1){
+        parent[nei] = node;
+        apexes++;
+        
+        dfs(nei);
+        lowlink[node] = min(lowlink[node],lowlink[nei]);
+        
+        if(lowlink[nei] > disc[node]) answer.push_back({nei,node});
+        
+      }else if(nei != parent[node])
+              lowlink[node] = min(lowlink[node], disc[nei]); 
+    }
+    
+    
+  vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
+    time = 0;
+    disc.resize(n, -1);
+    lowlink.resize(n,-1);
+    parent.resize(n,-1);
+    g.resize(n,vector<int>());
+    
+    for(auto &it:connections)
+      g[it[0]].push_back(it[1]), g[it[1]].push_back(it[0]);
+        
+    for(int i = 0; i != n; i++)
+      if(disc[i] == -1) dfs(i);
+        
+    return answer;
+  }
+};
