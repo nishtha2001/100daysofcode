c45323419187542634e5764f0f84f75ae72f8e4e
Nishtha Singh <75165796+nishtha2001@users.noreply.github.com>
2022-05-29 00:00:01 +0530

    Time: 22 ms (72.29%), Space: 18 MB (20.98%) - LeetHub

---
+++ 268-missing-number/268-missing-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int i=0;
+        sort(nums.begin(),nums.end());
+        for(i=0;i<nums.size();i++){
+          if( i == nums[i])
+            continue;
+        return i;    
+        }
+        return i;
+    }
+};
