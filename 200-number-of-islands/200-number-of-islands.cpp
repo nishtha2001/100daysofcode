1ef55edc4e213930d98c16472c8130298e32f051
Nishtha Singh <75165796+nishtha2001@users.noreply.github.com>
2022-06-20 21:50:35 +0530

    Time: 83 ms (14.59%), Space: 12.3 MB (58.75%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -6,7 +6,7 @@ public:
         
         if(grid[i][j] == '2' || grid[i][j] == '0')
             return;
-        
+    
         grid[i][j] = '2';
         
         DFS(grid, i+1, j);
@@ -17,16 +17,16 @@ public:
     
     int numIslands(vector<vector<char>>& grid) {
         
-        int islands = 0;
+        int ans = 0;
         for(int i = 0; i < grid.size(); i++) {
             for(int j = 0; j < grid[0].size(); j++) {
                
                 if(grid[i][j] == '1') {
                     DFS(grid, i, j);
-                    ++islands;
+                    ++ans;
                 } 
             }
         }
-        return islands;
+        return ans;
     }
 };
