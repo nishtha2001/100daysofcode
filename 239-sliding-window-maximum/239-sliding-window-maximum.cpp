c62c08712918b1e631d76528da58dcf2f2bbc8fd
Nishtha Singh <75165796+nishtha2001@users.noreply.github.com>
2022-08-23 12:53:29 +0530

    Time: 404 ms (72.19%), Space: 134.5 MB (92.37%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/239-sliding-window-maximum.cpp
@@ -1,42 +1,27 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        deque<int> q;
-        vector<int> res;
-        int i{}, j{}, maxVal = INT_MIN, n = nums.size();
+        vector<int>ans;
+        deque<int>d;
+        int i=0, j=0;
+        int n=nums.size();
         
-        //edge case - if k > n, return maxOf(arr)
-        if(k > n)
-        {
-            res.emplace_back(*max_element(nums.begin(), nums.end()));
-            return res;
-        }
         
-        while(j < n)
-        {
-            //calculations
-            while(!q.empty() && q.back() < nums.at(j))
-                q.pop_back();
-            
-            q.push_back(nums.at(j));
-            
-            if(j - i + 1 < k)
-                j++;
-            
-            else if(j - i + 1 == k)
-            {
-                maxVal = q.front();
-                res.emplace_back(maxVal);
+        while(j<n){
+            while(!d.empty() && d.back() < nums[j])
+                d.pop_back();
+            d.push_back(nums[j]);
+            if(j-i+1<k) j++;
+            else if(j-i+1==k){
+                ans.push_back(d.front());
                 
-                if(maxVal == nums.at(i) && !q.empty())
-                    q.pop_front();
+                if(d.front()==nums[i] && !d.empty())
+                    d.pop_front();
                 
                 i++;
                 j++;
             }
         }
-        
-        return res;
+        return ans;
     }
-    
 };
